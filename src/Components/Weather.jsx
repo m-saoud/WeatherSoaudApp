0ecb7aa0de7d2e72f0027b700335e8df3e8cb1c4
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-18 21:05:42 +0900

    Fetch weather data

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -3,15 +3,33 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Weather = () => {
-    const [weatherData, setWeatherData] = useState(null);
-    
-useEffect(()=>{},[])
-    return <div>
-      
-              {/* Display weather data here */}
+  const [weatherData, setWeatherData] = useState(null);
 
+  useEffect(() => {
+    const API_KEY = "7ec2036af2aa49fb22c35b29bd003d55";
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${API_KEY}`;;
 
-  </div>;
+    axios
+      .get(API_KEY)
+      .then((resp) => {
+        setWeatherData(resp.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching weather data:", err);
+      });
+  }, []);
+    return
+    <div>
+    {weatherData ? (
+      <div>
+        <h2>Weather in {weatherData.name}</h2>
+        <p>Temperature: {weatherData.main.temp}°C</p>
+        <p>Description: {weatherData.weather[0].description}</p>
+      </div>
+    ) : (
+      <p>Loading weather data...</p>
+    )}
+  </div>
 };
 
 export default Weather;
