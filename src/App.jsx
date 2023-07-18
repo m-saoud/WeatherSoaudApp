f98243433c99033abcca5bb576a9925ca1a343cc
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-18 21:07:56 +0900

    Run Weater comp into APP

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Weather from './Components/Weather'
 
 function App() {
   
 
   return (
     <>
-     <h1>Weather App </h1>
+      <h1>Weather App </h1>
+      <Weather />
+
     </>
   )
 }
