dee6dce8a6e1ab915b2e57a3cb691fef9149c9d5
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-18 21:16:46 +0900

    reclaim the render

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -7,7 +6,7 @@ const Weather = () => {
 
   useEffect(() => {
     const API_KEY = "7ec2036af2aa49fb22c35b29bd003d55";
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${API_KEY}`;;
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${API_KEY}`;
 
     axios
       .get(API_KEY)
@@ -18,18 +17,19 @@ const Weather = () => {
         console.error("Error fetching weather data:", err);
       });
   }, []);
-    return
-    <div>
-    {weatherData ? (
-      <div>
-        <h2>Weather in {weatherData.name}</h2>
-        <p>Temperature: {weatherData.main.temp}°C</p>
-        <p>Description: {weatherData.weather[0].description}</p>
-      </div>
-    ) : (
-      <p>Loading weather data...</p>
-    )}
-  </div>
+  return(<div>
+      {weatherData ? (
+        <div>
+          <h2>Weather in {weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+        </div>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  )
+  
 };
 
 export default Weather;
