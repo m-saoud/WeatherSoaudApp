8eacce2a807af38f0cc32ca9fb56ae3cd0c5c928
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-18 20:29:56 +0900

    build the main structure

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const Weather = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Weather
